7dfe78ce5f111b283b56785ca98db98c848b23aa
Andy Nguyen <andy.nguyen@championdata.com.au>
2024-10-14 20:31:57 +0700

    .

--- src/main/java/com/onlyu/boot2/CustomJerseyFilter.java
+++ src/main/java/com/onlyu/boot2/CustomJerseyFilter.java
@@ -1,28 +1,35 @@
 package com.onlyu.boot2;
 
-import javax.ws.rs.WebApplicationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
-import java.util.Map;
 
 @Provider
 public class CustomJerseyFilter implements ContainerRequestFilter, ContainerResponseFilter
 {
+    @Context
+    private HttpServletRequest httpServletRequest;
+
+    @Context
+    private HttpServletResponse httpServletResponse;
+
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException
     {
         System.out.println("Jersey ContainerRequestFilter...");
-        throw new WebApplicationException("Test exception from filter", Response.status(Response.Status.BAD_REQUEST.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(Map.of("errorMessage", "Test exception from filter")).build());
+        //throw new WebApplicationException("Test exception from filter", Response.status(Response.Status.BAD_REQUEST.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(Map.of("errorMessage", "Test exception from filter")).build());
     }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException
     {
+        System.out.println(httpServletRequest.getRemoteAddr());
+        httpServletResponse.setHeader("foo", "bar");
     }
 }
